bbb55e3e8fda8cdffea530ad1b818b24e8f5841c
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-01-13 17:43:54 +0900

    styled component자체를 타겟팅하기

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -26,7 +30,7 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  span {
+  ${Emoji} {
     font-size: 36px;
     &:hover {
       font-size: 100px;
@@ -38,8 +42,9 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>😆</span>
+        <Emoji as="p">😆</Emoji>
       </Box>
+      <Emoji>🔥</Emoji>
     </Wrapper>
   );
 }
