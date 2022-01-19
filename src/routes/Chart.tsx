98fe104c1e301d0fa5afbef29fecc827574b8def
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-01-19 12:22:58 +0900

    useQuery() refetchInterval & react-helmet

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -19,8 +19,10 @@ interface ChartProps {
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
-    fetchCoinHistory(coinId)
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    { refetchInterval: 10000 }
   );
   console.log(data);
   return (
